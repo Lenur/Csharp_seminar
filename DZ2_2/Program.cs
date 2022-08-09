cc19b6d77a8979b8ab4f61c87b2ffa692bf4b8f9
Lenur <blef07@gmail.com>
2022-08-09 23:37:46 +0300

    add DZ2_2

---
+++ DZ2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+string thirdNum = Convert.ToString(Math.Abs(num));  //Защита от ошибок отрицательных чисел
+
+if (thirdNum.Length > 2)    
+{
+    Console.WriteLine($"Третья цифра заданного числа равна {thirdNum[2]}");
+}
+else Console.WriteLine("Третьей цифры нет !");
