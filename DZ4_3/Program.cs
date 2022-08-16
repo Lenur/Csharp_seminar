09ff57270dc702eae764254d47b62ccd6c435b47
Lenur <blef07@gmail.com>
2022-08-17 00:36:32 +0300

    add DZ4_3

---
+++ DZ4_3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+//Метод создает массив
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+//Метод выводит массив
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] arr =new int[8];
+FillArray(arr);                 //создает массив
+Console.Write("Вывод массива: ");
+PrintArray(arr);                //выводит массив
