27e8506b6cd72f1095742857d8db87301786f7f6
Lenur <blef07@gmail.com>
2022-08-23 16:32:14 +0300

    add DZ5_2

---
+++ DZ5_2/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+//Метод заполнения массива случайными числами
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-100, 100);
+    }
+}
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+int sum = 0;
+FillArray(array);
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+        sum += array[i];
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях массива равна {sum}");
