1ad455c5f7724acc7b840acfff933d353a6cbc82
Lenur <blef07@gmail.com>
2022-09-08 20:06:32 +0300

    add DZ9_1

---
+++ DZ9_1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+(Решать методом рекурсии, на семинаре вторая задача похожа)
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""
+ */
+Console.Write("Введите число М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(PrintNumbers(m, n));
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return (start + "," + PrintNumbers(start + 1, end));
+}
